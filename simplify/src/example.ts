2638cf0e98d892a4db92b3ab0df04039a4ad5ffe
laj <larry.jones@reveal-energy.com>
2022-08-18 08:51:11 -0500

    Producer returns original if no changes

---
+++ simplify/src/example.ts
@@ -0,0 +1,24 @@
+import produce from 'immer';
+
+const todos = [
+    {
+        text: 'learn immer',
+        done: false,
+    },
+    {
+        text: 'simplify all code',
+        done: false,
+    },
+];
+
+const nextTodos = produce(todos, (draft) => {
+
+});
+
+console.log(nextTodos.length);
+console.log(todos[0].done);
+console.log(nextTodos[0].done);
+
+console.log(nextTodos === todos);
+console.log(nextTodos[0] === todos[0]);
+console.log(nextTodos[1] === todos[1]);
